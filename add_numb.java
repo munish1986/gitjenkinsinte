47eef96b09619c2f2e758d3e9b00b5e4972e35a3
munish1986 <munish.cet@gndu.ac.in>
2022-02-15 16:02:38 +0530

    programchanged

--- add_numb.java
+++ add_numb.java
@@ -2,9 +2,7 @@
 import java.util.Scanner;
 public class add_numb {
 	public static void main(String[] args) {
-	    int x=1, y=2, sum;
-	    sum = x + y;  // Calculate the sum of x + y
-	    System.out.println("Sum is: " + sum); // Print the sum
+	    	    System.out.println("hello"); // Print the sum
 	  }
 
 }
