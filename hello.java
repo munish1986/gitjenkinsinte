3146047f4a985e69f248b02d33c3ace30f32f7ee
munish1986 <munish.cet@gndu.ac.in>
2022-02-15 12:32:50 +0530

    duplication added to program

--- hello.java
+++ hello.java
@@ -3,8 +3,8 @@ public class hello {
 	 public static void main(String[] args)
 	    {
 	    System.out.println("HELLO JAVA");
-	    System.out.println("HELLO JAVA");
-	    System.out.println("HELLO JAVA");
-	    }
+	    	    }
+System.out.println("HELLO outside JAVA");
+System.out.println("HELLO  outside JAVA");
 
 }
